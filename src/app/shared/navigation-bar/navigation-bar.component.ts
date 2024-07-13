34bb67916f5fb96b3df60efd3210e7f2303c1f50
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2024-07-12 20:10:25 -0400

    fix navigation part 4

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { Router, NavigationEnd, RouterLinkActive, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -9,5 +10,28 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
+  @ViewChild('navbarToggler') navbarToggler!: ElementRef;
+  @ViewChild('navbarSupportedContent') navbarContent!: ElementRef;
 
+  constructor(private router: Router, private renderer: Renderer2) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.closeNavbar();
+    });
+  }
+
+  closeNavbar() {
+    if (this.navbarContent && this.navbarToggler) {
+      const content = this.navbarContent.nativeElement;
+      const toggler = this.navbarToggler.nativeElement;
+
+      if (content.classList.contains('show')) {
+        setTimeout(() => {
+          this.renderer.removeClass(content, 'show');
+          toggler.setAttribute('aria-expanded', 'false');
+        }, 150); // Small delay to ensure the navigation has occurred
+      }
+    }
+  }
 }
